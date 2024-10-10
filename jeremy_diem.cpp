80c31f05bccb304a6d956f6dbf2e9f9897173cd6
JeremiasDIaz99 <diemje01@thu.de>
2024-10-10 21:32:26 +0200

    added jeremy_diem cpp/h

---
+++ jeremy_diem.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void print_info_jeremy_diem() {
+    std::cout << "Jeremy Diem";
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "alton_bekolli.h"
+#include "jeremy_diem.h"
 
 int main(int, char**) {
     std::cout << "Unsere Gruppe:\n\n";
     print_info_alton_bekolli();
+    print_info_jeremy_diem();
+
 }
