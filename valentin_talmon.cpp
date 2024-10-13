f91dc77f1e829f775b4f05af2055d2fb154f55f2
Valentin Talmon-l'Armée <talmva01@thu.de>
2024-10-13 14:32:21 +0200

    Header/cpp file for name

---
+++ valentin_talmon.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ValeT on 13.10.2024.
+//
+#include <iostream>
+using namespace std;
+
+void print_info_valentin_talmon() {
+    cout << "Valentin Talmon-l'Armée \n";
+}
---
+++ valentin_talmon.h
@@ -0,0 +1,9 @@
+//
+// Created by ValeT on 13.10.2024.
+//
+
+#ifndef VALENTIN_TALMON_H
+void print_info_valentin_talmon();
+#define VALENTIN_TALMON_H
+
+#endif //VALENTIN_TALMON_H
